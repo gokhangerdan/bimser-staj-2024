50e6d1f7879613f06a0ef41851f2bcd8abad36d6
SualpGokalp <sgokalp5995@hotmail.com>
2024-07-21 23:38:47 +0300

    s

--- streamlit-app/fastapi/myapi2.py
+++ streamlit-app/fastapi/myapi2.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -14,3 +15,17 @@ def index(limit = 10, published: bool = True, sort: Optional[str] = None):
 @app.get("/blog/{id}")
 def show(id):
     return {"data": id} 
+
+@app.get("/blog/{id}/comments")
+def comments(id, limit=10):
+    return{"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return{"data": f"Blog is created with title as{request.title}"}
