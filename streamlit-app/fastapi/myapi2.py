25812be1f0fdb4547150ec0798470857c9b6ee2c
SualpGokalp <sgokalp5995@hotmail.com>
2024-07-24 22:07:49 +0300

    s

--- streamlit-app/fastapi/myapi2.py
+++ streamlit-app/fastapi/myapi2.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-
+import uvicorn
 
 app = FastAPI()
 
@@ -29,3 +29,6 @@ class Blog(BaseModel):
 @app.post("/blog")
 def create_blog(request: Blog):
     return{"data": f"Blog is created with title as{request.title}"}
+
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="127.0.0.1", port= "9000")
